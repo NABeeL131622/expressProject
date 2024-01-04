94499b6d8e07e0a1a074cd147eaa928b7c0ab2fc
Nabeel <nabeelma2000@gmail.com>
2024-01-04 09:16:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const contactRouter = require('./routes/contactRoutes')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
+app.use('/api/contacts',contactRouter)
+
+// app.get('/api/contacts',(req,res)=>{
+//     res.status(200).json({message:'get all contacts'})
+// })
+
